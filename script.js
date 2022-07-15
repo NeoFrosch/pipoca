df628d53fa1dae5b37fccf3ca01e81dbc8811e7f
Isabella Miki Kobayachi <isabellamiki2004@gmail.com>
2022-07-15 10:50:11 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,94 @@
+console.log("aoba")
+
+let clicks = 0;
+
+function braum (){
+  clicks = clicks + 1;
+  counter.innerHTML = clicks;
+
+  if (clicks == 10) {
+    document.getElementById("conq1").innerHTML = "Um dia eu perguntei pra Ana quando era seu aniversauro, ela não sabia, ela ativou o modo FBI e descobriu em 5minutos"
+    console.log("que Jesus Karl abençoe a pipoca e você")
+    }
+
+  if(clicks == 15) {
+    document.getElementById("conq2").innerHTML = "Atexandre Hodeki Jiromba é um nome Dikcio"
+    console.log("japones da federal,")
+  }
+
+  if(clicks == 20) {
+    document.getElementById("conq3").innerHTML = "A maconha pode estragar sua vida, principalmente se for a Maconha de Modelar"
+    console.log("as vezes assassina o português, como o inesquecível &#34;a Jente &#34; e a massinha com C e O");
+  }
+
+  if(clicks == 25) {
+    document.getElementById("conq4").innerHTML = "Maior bebum do Japão Vertical"
+    console.log("pra que comer pastel com caldo de cana se pode comer pastel com whisky")
+  }
+
+  if(clicks == 30) {
+    document.getElementById("conq5").innerHTML = "Maníaco dos pedreiros"
+    console.log("https://discord.com/channels/768991176916992080/768991176916992083/942496466113527858")
+  }
+
+  if(clicks == 35) {
+    document.getElementById("conq6").innerHTML = "E até hoje o Brunão não pagou os pedreiros prometidos"
+    console.log("aproveita que é seu dia, chega no zap zap do Brunão e cobra todos os pedreiros que ela prometeu")
+  }
+
+  if(clicks == 40) {
+    document.getElementById("conq7").innerHTML = "vOcÊ gOsTa De pIpOcA?"
+    console.log("o Jubileu é o maior inimigo dos pipoqueiros ou ele é parça?")
+  }
+
+  if(clicks == 45) {
+    document.getElementById("conq8").innerHTML = "Um trio de Magneton é um Magnetrio?"
+  }
+
+  if(clicks == 50) {
+    document.getElementById("conq9").innerHTML = "&#34;To vendo a bunda do Deki&#34;- Danse, AlosOn"
+  }
+
+  if(clicks == 55) {
+    document.getElementById("conq10").innerHTML = "BEM VINDO REFLECT SUP?"
+    console.log("SUP?")
+  }
+
+  if(clicks == 60) {
+    document.getElementById("conq11").innerHTML = "Se liga &#127871;"
+  }
+
+  if(clicks == 65) {
+    document.getElementById("conq12").innerHTML = "Desgraçado da corrente"
+  }
+
+  if(clicks == 70) {
+    document.getElementById("conq13").innerHTML = "Lendário Dekissauro no Gmod que entalava em tudo"
+  }
+
+  if(clicks == 90) {
+    document.getElementById("conq14").innerHTML = "Pastel de Pipoca"
+    console.log("criatividade ta acabando já...")
+  }
+
+  if(clicks == 100) {
+    document.getElementById("conq15").innerHTML = "&#34; O que os olhos não veem, a mão não coça pra comprar&#34; - Siromba, Atexandre Ri de que?"
+    console.log("deixando a zueira de lado, fazer esse pipoclicker foi divertido demais,")
+  }
+
+  if(clicks == 666) {
+    document.getElementById("conq16").innerHTML = "meu deus meu senhor me ajuda por favor"
+    console.log("a programação é uma área apaixonante como você disse, nunca imaginei que eu iria conseguir fazer um site de pipoca para um pipoqueiro")
+  }
+
+  if(clicks == 700) {
+    document.getElementById("conq1").innerHTML = "achou que tinha acabado? Achou errado, olha o console :braum:"
+    console.log("por fim, já que minha criatividade esgotou, feliz aniversauro, espero que encha o buxim de pedreiro, pipoca e whiskas!")
+  }
+
+  if(clicks == 1000) {
+    alert("quebrou o mouse?")
+
+}
+}
+
